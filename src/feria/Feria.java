dacd60f6b4741dac87e7c4498c9d6798b56aedbc
luisa_Arango1408 <luisa.arango@mariaelvinia.edu.co>
2020-09-28 18:19:55 -0500

    Version 1.0
    Creacion de la clase Feria, de sus metodos get y set del metodo consstructory de dos metodos adicionales.
    
    Se instanciaron dos objetos a partir de la clase

--- src/feria/Feria.java
+++ src/feria/Feria.java
@@ -1,21 +1,115 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package feria;
 
-/**
- *
- * @author usuario
- */
-public class Feria {
+import java.util.Date;
 
+
+public class Feria {
+    
+   private String nombre;
+   private int numerodeTrabajadores; 
+   private Date fechaDeFundacion;
+   private String especialidad;
+   private Boolean nacional;
+
+    public Feria(
+            String nombre, 
+            int numerodeTrabajadores, 
+            Date fechaDeFundacion, 
+            String especialidad, 
+            Boolean nacional) {
+        this.nombre = nombre;
+        this.numerodeTrabajadores = numerodeTrabajadores;
+        this.fechaDeFundacion = fechaDeFundacion;
+        this.especialidad = especialidad;
+        this.nacional = nacional;
+    }
+   
     /**
-     * @param args the command line arguments
+    *El metodo aumenta la cantidad de trabajadores de la feria en 100
+    *y la conviente en nacional
+    */
+    public void ampliarFeria(){
+        this.numerodeTrabajadores +=100;
+        this.nacional =true;  
+    }
+    
+    /**
+     * Este metodo permite cambiar el nombre de la feria y su especialidad
      */
+    public void reinaguracion(
+            String nuevoNombre,
+            String numeroEspecialidad){
+        this.nombre = nuevoNombre;
+        this.especialidad = numeroEspecialidad;
+        this.numerodeTrabajadores +=1;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int setNumerodeTrabajadores() {
+        return numerodeTrabajadores;
+    }
+
+    public void setNumerodeTrabajadores(int numerodeTrabajadores) {
+        this.numerodeTrabajadores = numerodeTrabajadores;
+    }
+
+    public Date getFechaDeFundacion() {
+        return fechaDeFundacion;
+    }
+
+    public void setFechaDeFundacion(Date fechaDeFundacion) {
+        this.fechaDeFundacion = fechaDeFundacion;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Boolean getNacional() {
+        return nacional;
+    }
+
+    public void setNacional(Boolean nacional) {
+        this.nacional = nacional;
+    }
+   
+   
+   
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Feria feriaUno = new Feria(
+                "Peaceful taste",
+                280,
+                new Date(),
+                "Comida tipica paisa",
+                false);
+        Feria feriaDos = new Feria(
+                "Makeup and cares",
+                15,
+                new Date(),
+                "Maquiilaje y jabones",
+                true);
+        System.out.println(feriaUno.getNombre());
+        System.out.println(feriaUno.setNumeroDeTrabajadores());
+        System.out.println(feriaUno.getNacional());
+        
+        feriaUno.ampliarFeria();
+        
+        System.out.println(feriaUno.getNombre());
+        System.out.println(feriaUno.setNumeroDeTrabajadores());
+        System.out.println(feriaUno.getNacional());
     }
     
 }
